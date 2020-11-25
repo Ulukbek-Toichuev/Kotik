27de7f87a20e14815944d5de2b41cc74f6dd8bcf
Toychuev. Ulukbek <t.ulukbek01@gmail.com>
2020-11-26 02:35:11 +0300

    update my 5 methods (fix all methods). Upgrade eat(), liveAnotherDay() method.

--- src/Application.java
+++ src/Application.java
@@ -3,10 +3,8 @@ import model.Kotik;
 public class Application {
     public static void main(String[] args) {
         Kotik kotik = new Kotik(10, 3,  "bob", "Meow!");
-        kotik.chaseMouse();
-        kotik.goForAWalk();
-        kotik.goToTheCatLitter();
-        kotik.play();
-        kotik.sleep();
+
+        kotik.eat();
+
     }
 }
--- src/model/Kotik.java
+++ src/model/Kotik.java
@@ -1,11 +1,14 @@
 package model;
+import java.util.Scanner;
+
 public class Kotik {
     private int prettiness;
     private int weight;
     private String name;
     private String meow;
-    private int satiety;
-
+    private final int satiety = (int) (Math.random() *(2+1)) - 1;
+    private final String hungry= "Судя по тому как он истошно мяукает, думаю его стоит покормить.";
+    private final String full= "Есть он сейчас точно не хочет.";
     // Конструктор без параметров, он идентичен конструктору по умолчанию,
     // который сюда бы подставила Java,
     // Но она его не подставит, по той причине, 
@@ -28,49 +31,157 @@ public class Kotik {
         this.meow = meow;
     }
 
-    //get-set satiety
-    public void setSatiety(int satiety) {
-        this.satiety = satiety;
-    }
-    public int getSatiety() {
+    public int eat() {
+        if (satiety <= 0) {
+            int chois;
+            System.out.println(hungry + "\n" + "Покормите кота!");
+            System.out.println("Нажмите 0 чтобы выбрать Сухой корм" + "\n"
+                    + "Нажмите 1 чтобы выбрать Консервы" + "\n" + "Нажмите 2 чтобы выбрать Натуральная еда");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                chois = scanner.nextInt();
+            } else {
+                System.out.println("Вы ввели не число! Повторите ввод!");
+                return eat();
+            }
+            if (chois > 2){
+                System.out.println( "Вы ввели число больше 2ух!");
+                return eat();
+            }
+
+            switch (chois) {
+                case 0:
+                    System.out.println("Котику понравился ваш выбор теперь он сытый " +
+                            "практически на целый день!");
+                    break;
+                case 1:
+                    System.out.println("Котик: кушает с большим аппетитом.");
+                    break;
+                case 2:
+                    System.out.println("Натуральная еда, то что надо для " +
+                            "здорового образа жизни. Ваш питомец тоже так считает, " +
+                            "судя по тому как он всё скушал быстро.");
+                    break;
+            }
+        } else if (satiety > 0) {
+            System.out.println(full);
+        }
         return satiety;
     }
 
-    public boolean play(){
-        boolean bool = true;
-        if (bool)
-            System.out.println("Play");
-        return bool;
+        public  boolean play(){
+            int a = (int) (Math.random() * 2 + 1);
+            switch (a) {
+                case 0:
+                    System.out.println("Котик играется с мягкой игрушкой.");
+                    break;
+                case 1:
+                    System.out.println("Котик играется с хозяином.");
+                    break;
+            }
+            return true;
+        }
+
+        public boolean sleep () {
+        int a = (int) (Math.random() * 2 + 1);
+            switch (a) {
+                case 0:
+                    System.out.println("Котик спит на диване.");
+                    break;
+                case 1:
+                    System.out.println("Котик спит под кроватью.");
+                    break;
+            }
+            return true;
+        }
+
+        public boolean chaseMouse () {
+            int a = (int) (Math.random() * 2 + 1);
+            switch (a) {
+                case 0:
+                    System.out.println("Котик гоняется за мышью во дворе.");
+                    break;
+                case 1:
+                    System.out.println("Котик гоняется за мышью в подвале.");
+                    break;
+            }
+            return true;
+        }
+
+        public boolean goToTheCatLitter () {
+            int a = (int) (Math.random() * 2 + 1);
+            switch (a) {
+                case 0:
+                    System.out.println("Котик решил сходить в лоток по маленькому.");
+                    break;
+                case 1:
+                    System.out.println("Котик решил сходить в лоток по большому.");
+                    break;
+            }
+            return true;
+        }
+
+        public boolean goForAWalk () {
+            int a = (int) (Math.random() * 2 + 1);
+            switch (a) {
+                case 0:
+                    System.out.println("Котик гуляет с хозяином по парку.");
+                    break;
+                case 1:
+                    System.out.println("Котик гуляет сам по двору.");
+                    break;
+            }
+            return true;
+        }
+
+        public void liveAnotherDay () {
+
+            if (satiety <= 0) {
+                eat();
+
+                for (int i = 0; i < 24; i++)
+                    System.out.print("#");
+                    System.out.println(" ");
+
+                int a = (int) (Math.random() * 5 + 1);
+                switch (a) {
+                    case 0:
+                        System.out.println(goForAWalk());
+                        break;
+                    case 1:
+                        System.out.println(sleep());
+                        break;
+                    case 2:
+                        System.out.println(play());
+                        break;
+                    case 3:
+                        System.out.println(goToTheCatLitter());
+                        break;
+                    case 4:
+                        System.out.println(chaseMouse());
+                        break;
+                }
+            } else if (satiety > 0) {
+                int a = (int) (Math.random() * 5 +1);
+                switch (a) {
+                    case 0:
+                        System.out.println(goForAWalk());
+                        break;
+                    case 1:
+                        System.out.println(sleep());
+                        break;
+                    case 2:
+                        System.out.println(play());
+                        break;
+                    case 3:
+                        System.out.println(goToTheCatLitter());
+                        break;
+                    case 4:
+                        System.out.println(chaseMouse());
+                        break;
+                }
+            }
+
+
+        }
     }
-
-    public boolean sleep(){
-        boolean bool = true;
-        if (bool)
-            System.out.println("Sleep");
-        return bool;
-    }
-
-    public boolean chaseMouse(){
-        boolean bool = true;
-        if (bool)
-            System.out.println("Chase mouse");
-        return bool;
-    }
-
-    public boolean goToTheCatLitter(){
-        boolean bool = true;
-        if (bool)
-            System.out.println("Go to the cat litter");
-        return bool;
-    }
-
-    public boolean goForAWalk(){
-        boolean bool = true;
-        if (bool)
-            System.out.println("Go for a walk");
-        return bool;
-    }
-
-
-
-}
