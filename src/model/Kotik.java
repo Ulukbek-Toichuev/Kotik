e09125d9ba47f09d14e2a6c202388cabc9871b49
Toychuev. Ulukbek <t.ulukbek01@gmail.com>
2020-11-25 03:03:26 +0300

    add set-get and have warning with methods (because not have 'return')

--- src/model/Kotik.java
+++ src/model/Kotik.java
@@ -1,8 +1,10 @@
+package model;
 public class Kotik {
-    int prettiness;
-    int weight;
-    String name;
-    String meow;
+    private int prettiness;
+    private int weight;
+    private String name;
+    private String meow;
+    private int satiety;
 
     // Конструктор без параметров, он идентичен конструктору по умолчанию,
     // который сюда бы подставила Java,
@@ -26,11 +28,67 @@ public class Kotik {
         this.meow = meow;
     }
 
+    /// get-set Prettiness
+    public int getPrettiness(){
+        return prettiness;
+    }
 
-    void setKotik(int prettiness, String name, int weight, String meow) {
+    public void setPrettiness(int prettiness){
         this.prettiness = prettiness;
+    }
+
+    ///get-set Weight
+    public int getWeight(){
+        return weight;
+    }
+
+    public void setWeight(int weight){
         this.weight = weight;
-        this.name = name;
+    }
+
+    ///get-set Name
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String a){
+        this.name = a;
+    }
+
+    /// get-set Meow
+    public String getMeow(){
+        return meow;
+    }
+
+    public void setMeow(String meow){
         this.meow = meow;
     }
+
+    public boolean play()
+    {
+
+    }
+
+    public boolean sleep()
+    {
+
+    }
+
+    public boolean chaseMouse()
+    {
+
+    }
+
+    public boolean goToTheCatLitter()
+    {
+
+    }
+
+    public boolean goForAWalk()
+    {
+
+    }
+
+
+
 }
